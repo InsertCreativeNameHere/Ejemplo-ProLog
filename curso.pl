623c3f07b3bded83480579c821701986b187fe17
Javier Aponte <48999640+InsertCreativeNameHere@users.noreply.github.com>
2020-03-04 09:25:09 -0500

    Add files via upload

---
+++ curso.pl
@@ -0,0 +1,203 @@
+%Presentado por: Javier Aponte 20172020036
+%		 Daniel Hormiga 20172020026
+%		 Johan Perez 20161020055
+
+% Lista de estudiantes
+esEstudiante('David Borrajo').
+esEstudiante('Jose Mata').
+esEstudiante('Oscar Sanchez').
+esEstudiante('Ernesto Triana').
+esEstudiante('Andres Villamil').
+esEstudiante('Angel Urbina').
+esEstudiante('Manuel Espinar').
+esEstudiante('Jesus Ortigosa').
+esEstudiante('Emilio Sena').
+esEstudiante('Carlos Arboleda').
+esEstudiante('Sofia Aponte').
+esEstudiante('Marcos Torres').
+esEstudiante('Hanna Hernandez').
+esEstudiante('Laura Lopez').
+esEstudiante('Dayana Rangel').
+esEstudiante('Lorena Chavarro').
+esEstudiante('Jenifer Velez').
+esEstudiante('Daniel Hormiga').
+esEstudiante('Javier Aponte').
+esEstudiante('Johan Perez').
+
+%Lista de materias
+
+esMateria('Modelos 2').
+esMateria('Ciencias').
+esMateria('Calculo').
+
+%Lista de estudiantes de cada materia
+
+%Modelos 2
+
+estaInscrito('Javier Aponte','Modelos2').
+estaInscrito('Daniel Hormiga','Modelos2').
+estaInscrito('Johan Perez','Modelos2').
+estaInscrito('Lorena Chavarro','Modelos2').
+estaInscrito('Angel Urbina','Modelos2').
+estaInscrito('Jose Mata','Modelos2').
+estaInscrito('Jenifer Velez','Modelos2').
+estaInscrito('Laura Lopez','Modelos2').
+
+%Ciencias
+
+estaInscrito('Dayana Rangel','Ciencias').
+estaInscrito('Javier Aponte','Ciencias').
+estaInscrito('Jesus Ortigosa','Ciencias').
+estaInscrito('Andres Villamil','Ciencias').
+estaInscrito('Johan Perez','Ciencias').
+estaInscrito('Marcos Torres','Ciencias').
+estaInscrito('Manuel Espinar','Ciencias').
+
+%Calculo
+
+estaInscrito('Emilio Sena','Calculo').
+estaInscrito('Carlos Arboleda','Calculo').
+estaInscrito('Daniel Hormiga','Calculo').
+estaInscrito('Johan Perez','Calculo').
+estaInscrito('Daniel Borrajo','Calculo').
+estaInscrito('Oscar Sanchez','Calculo').
+estaInscrito('Ernesto Triana','Calculo').
+estaInscrito('Sofia Aponte','Calculo').
+estaInscrito('Hanna Hernandez','Calculo').
+estaInscrito('Jenifer Velez','Calculo').
+
+%Cantidad de clases por materia
+
+cantClases('Modelos2',32).
+cantClases('Ciencias',48).
+cantClases('Calculo',64).
+
+%Lista de asistencias
+
+%Calculo
+cantFallas('Emilio Sena','Calculo',0).
+cantFallas('Carlos Arboleda','Calculo',10).
+cantFallas('Daniel Hormiga','Calculo',5).
+cantFallas('Johan Perez','Calculo',2).
+cantFallas('Daniel Borrajo','Calculo',15).
+cantFallas('Oscar Sanchez','Calculo',25).
+cantFallas('Ernesto Triana','Calculo',8).
+cantFallas('Sofia Aponte','Calculo',0).
+cantFallas('Hanna Hernandez','Calculo',4).
+cantFallas('Jenifer Velez','Calculo',32).
+
+%Ciencias
+
+cantFallas('Dayana Rangel','Ciencias',1).
+cantFallas('Javier Aponte','Ciencias',3).
+cantFallas('Jesus Ortigosa','Ciencias',10).
+cantFallas('Andres Villamil','Ciencias',5).
+cantFallas('Johan Perez','Ciencias',15).
+cantFallas('Marcos Torres','Ciencias',8).
+cantFallas('Manuel Espinar','Ciencias',9).
+
+%Modelos2
+
+
+cantFallas('Javier Aponte','Modelos2',5).
+cantFallas('Daniel Hormiga','Modelos2',7).
+cantFallas('Johan Perez','Modelos2',3).
+cantFallas('Lorena Chavarro','Modelos2',12).
+cantFallas('Angel Urbina','Modelos2',0).
+cantFallas('Jose Mata','Modelos2',2).
+cantFallas('Jenifer Velez','Modelos2',6).
+cantFallas('Laura Lopez','Modelos2',0).
+
+%Notas
+
+%Modelos2
+
+nota('Javier Aponte','Modelos2','parcial1',3.5).
+nota('Javier Aponte','Modelos2','parcial2',4).
+nota('Javier Aponte','Modelos2','parcial3',3).
+nota('Daniel Hormiga','Modelos2','parcial1',5).
+nota('Daniel Hormiga','Modelos2','parcial2',4).
+nota('Daniel Hormiga','Modelos2','parcial3',4.5).
+nota('Johan Perez','Modelos2','parcial1',4).
+nota('Johan Perez','Modelos2','parcial2',3.2).
+nota('Johan Perez','Modelos2','parcial3',4.7).
+nota('Lorena Chavarro','Modelos2','parcial1',2).
+nota('Lorena Chavarro','Modelos2','parcial2',3.5).
+nota('Lorena Chavarro','Modelos2','parcial3',1.8).
+nota('Angel Urbina','Modelos2','parcial1',4).
+nota('Angel Urbina','Modelos2','parcial2',1.2).
+nota('Angel Urbina','Modelos2','parcial3',4.7).
+nota('Jose Mata','Modelos2','parcial1',5).
+nota('Jose Mata','Modelos2','parcial2',3.2).
+nota('Jose Mata','Modelos2','parcial3',3.7).
+nota('Jenifer Velez','Modelos2','parcial1',3.8).
+nota('Jenifer Velez','Modelos2','parcial2',4.5).
+nota('Jenifer Velez','Modelos2','parcial3',2).
+nota('Laura Lopez','Modelos2','parcial1',3).
+nota('Laura Lopez','Modelos2','parcial2',3).
+nota('Laura Lopez','Modelos2','parcial3',3).
+
+%Ciencias
+
+nota('Dayana Rangel','Ciencias','parcial1',4.4).
+nota('Dayana Rangel','Ciencias','parcial2',3.4).
+nota('Dayana Rangel','Ciencias','parcial3',2).
+nota('Javier Aponte','Ciencias','parcial1',1.2).
+nota('Javier Aponte','Ciencias','parcial2',2.5).
+nota('Javier Aponte','Ciencias','parcial3',3.7).
+nota('Jesus Ortigosa','Ciencias','parcial1',4.8).
+nota('Jesus Ortigosa','Ciencias','parcial2',1.8).
+nota('Jesus Ortigosa','Ciencias','parcial3',3.5).
+nota('Andres Villamil','Ciencias','parcial1',5).
+nota('Andres Villamil','Ciencias','parcial2',4.6).
+nota('Andres Villamil','Ciencias','parcial3',4.2).
+nota('Johan Perez','Ciencias','parcial1',3.5).
+nota('Johan Perez','Ciencias','parcial2',1.5).
+nota('Johan Perez','Ciencias','parcial3',2.9).
+nota('Marcos Torres','Ciencias','parcial1',4.1).
+nota('Marcos Torres','Ciencias','parcial2',1.6).
+nota('Marcos Torres','Ciencias','parcial3',2.8).
+nota('Manuel Espinar','Ciencias','parcial1',4.9).
+nota('Manuel Espinar','Ciencias','parcial2',5).
+nota('Manuel Espinar','Ciencias','parcial3',0).
+
+%Calculo
+
+nota('Emilio Sena','Calculo','parcial1',2).
+nota('Emilio Sena','Calculo','parcial2',1.5).
+nota('Emilio Sena','Calculo','parcial3',5).
+nota('Carlos Arboleda','Calculo','parcial1',4.5).
+nota('Carlos Arboleda','Calculo','parcial2',3.2).
+nota('Carlos Arboleda','Calculo','parcial3',5).
+nota('Daniel Hormiga','Calculo','parcial1',1.8).
+nota('Daniel Hormiga','Calculo','parcial2',2.6).
+nota('Daniel Hormiga','Calculo','parcial3',3).
+nota('Johan Perez','Calculo','parcial1',4.5).
+nota('Johan Perez','Calculo','parcial2',1.8).
+nota('Johan Perez','Calculo','parcial3',3.7).
+nota('Daniel Borrajo','Calculo','parcial1',3.4).
+nota('Daniel Borrajo','Calculo','parcial2',2.8).
+nota('Daniel Borrajo','Calculo','parcial3',3.8).
+nota('Oscar Sanchez','Calculo','parcial1',1).
+nota('Oscar Sanchez','Calculo','parcial2',0.5).
+nota('Oscar Sanchez','Calculo','parcial3',5).
+nota('Ernesto Triana','Calculo','parcial1',4.4).
+nota('Ernesto Triana','Calculo','parcial2',3.9).
+nota('Ernesto Triana','Calculo','parcial3',4.8).
+nota('Sofia Aponte','Calculo','parcial1',3.7).
+nota('Sofia Aponte','Calculo','parcial2',2.8).
+nota('Sofia Aponte','Calculo','parcial3',5).
+nota('Hanna Hernandez','Calculo','parcial1',4).
+nota('Hanna Hernandez','Calculo','parcial2',1.9).
+nota('Hanna Hernandez','Calculo','parcial3',3.4).
+nota('Jenifer Velez','Calculo','parcial1',2.4).
+nota('Jenifer Velez','Calculo','parcial2',3.9).
+nota('Jenifer Velez','Calculo','parcial3',4.6).
+
+%Funciones
+
+notaFinal(X,Y,N) :- nota(X,Y,'parcial1',A),nota(X,Y,'parcial2',B),nota(X,Y,'parcial3',C), N is A*0.35 + B*0.35 + C*0.3.    % Donde X es la materia y Y el estudiante
+
+inasistencia(X,Y,I) :- cantFallas(X,Y,F), cantClases(Y,T), I is (F/T)*100.
+
+aprueba(X,Y) :- notaFinal(X,Y,N),inasistencia(X,Y,I), N >= 2.95 , I < 20.
